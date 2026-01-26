e020f0fda56f501c3960799d68e43df2ce3b19e4
8octa <tataruoctavian5@gmail.com>
2026-01-26 23:23:14 +0200

    added gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
-    <section className="min-h-screen flex items-center">
-      <div className="max-w-6xl mx-auto px-6 space-y-6">
-        <p className="text-muted max-w-xl uppercase text-text/30 font-bold">
+    <section className="hero min-h-screen flex items-center bg-[url('/hero.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="max-w-6xl mx-auto p-6 space-y-6 bg-dark/50">
+        <p className="text-muted max-w-xl uppercase text-text/60 font-bold">
           Club de Arte Marțiale Zanshin Moreni
         </p>
-        <h1 className="text-5xl md:7xl uppercase tracking-widest">
+        <h1 className="text-5xl font-bold uppercase tracking-widest">
           Tradiție. Disciplina. Măiestrie.
         </h1>
 
@@ -13,7 +13,7 @@ export default function Hero() {
           Shōrin-ryū Karate · Wing Chun Kung Fu
         </p>
 
-        <button className="border border-accent px-8 py-4 uppercase tracking-widest hover:bg-accent transition">
+        <button className="border border-accent px-8 py-4 uppercase tracking-widest hover:bg-accent transition font-bold">
           <a href="http://" target="_blank" rel="noopener noreferrer">
             Vreau sa învăț!
           </a>
